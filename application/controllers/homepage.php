5409538ae93b89bab92d83fbc9157335f8f9854f
aleWaskitha <aleiawaskitha06@gmail.com>
2025-04-25 00:34:42 +0700

    Update homepage.php

--- application/controllers/homepage.php
+++ application/controllers/homepage.php
@@ -4,5 +4,17 @@ class Homepage extends CI_Controller{
     public function index(){
         $this->load->view('home/v_homepage');
     }
+
+    public function menu(){
+        $this->load->view('home/v_menu');
+    }
+
+    public function billiard(){
+        $this->load->view('home/v_billiard');
+    }
+
+    public function pingpong(){
+        $this->load->view('home/v_pingpong');
+    }
 }
 ?>
